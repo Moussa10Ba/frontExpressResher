9b76b89a7561a7ac9a31d280d2a8e091529c44aa
Moussa Ba <moussaba@Moussas-MacBook-Pro.local>
2025-04-06 19:28:06 -0600

    User Update

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,54 +1,126 @@
-import { useState, FormEvent, ChangeEvent, useEffect } from "react";
-import axios, { AxiosError } from "axios";
-import { useNavigate, useParams } from "react-router-dom";
+import { useState, FormEvent, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 import User from "../models/User";
 
 const RegisterForm = () => {
-
-  const {id} =  useParams();
+  const { id } = useParams();
   const [user, setUser] = useState<User>();
 
-  useEffect(()=>{
-    if(id){
+  useEffect(() => {
+    if (id) {
       console.log(id);
-      const getUser = async()=>{
-        const response =  await axios.get(`http://localhost:8000/api/users/${id}`);
+      const getUser = async () => {
+        const response = await axios.get(
+          `http://localhost:8000/api/users/${id}`
+        );
         setUser(response.data.user);
-        console.log(response.data.user);
-      }      
-     getUser();
+      };
+      getUser();
     }
-
   }, [id]);
 
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    try {
+      if (id) {
+        const res = await axios.put(`http://localhost:8000/api/users/${id}`, user);
+        if(res.status == 200){
+          window.alert('User Updated SuccesFully');
+        }
+        window.alert('Oups something went Wrong....');
+      } else {
+        await axios.post(`http://localhost:8000/api/users/`, user);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="container mt-5 w-50">
       <h2 className="text-center">{id ? "Updating User" : "New User"}</h2>
-      <form>
+      <form onSubmit={ handleSubmit}>
         <div className="mb-3">
           <label className="form-label">First Name</label>
-          <input type="text" className="form-control form-control-lg" value={user?.firstName}/>
+          <input
+            type="text"
+            className="form-control form-control-lg"
+            value={user?.firstName}
+            onChange={(e) =>
+              setUser({
+                ...user as User,
+                firstName: e.target.value,
+              })
+            }
+          />
         </div>
         <div className="mb-3">
           <label className="form-label">Last Name</label>
-          <input type="text" className="form-control form-control-lg"  value={user?.lastName}/>
+          <input
+            type="text"
+            className="form-control form-control-lg"
+            value={user?.lastName}
+            onChange={(e) => {
+              setUser({
+                ... user as User,
+                lastName: e.target.value,
+              });
+            }}
+          />
         </div>
         <div className="mb-3">
           <label className="form-label">Email address</label>
-          <input type="email" className="form-control form-control-lg" value={user?.email}/>
+          <input
+            type="email"
+            className="form-control form-control-lg"
+            value={user?.email}
+            onChange={(e) =>
+              setUser({
+                ...user as User,
+                email: e.target.value,
+              })
+            }
+          />
         </div>
         <div className="mb-3">
           <label className="form-label">Phone Number</label>
-          <input type="text" className="form-control form-control-lg" value={user?.phoneNumber} />
+          <input
+            type="text"
+            className="form-control form-control-lg"
+            value={user?.phoneNumber}
+            onChange={(e) =>
+              setUser({
+                ...user as User,
+                phoneNumber: e.target.value,
+              })
+            }
+          />
         </div>
         <div className="mb-3">
           <label className="form-label">Adress</label>
-          <input type="text" className="form-control form-control-lg" value={user?.address} />
+          <input
+            type="text"
+            className="form-control form-control-lg"
+            value={user?.address}
+            onChange={(e) =>
+              setUser({
+                ...user,
+                address: e.target.value,
+              })
+            }
+          />
         </div>
-        <button type="submit" className="btn btn-primary btn-lg w-50 block ">
-          Save changes
-        </button>
+        {id ? (
+          <button type="submit" className="btn btn-primary btn-lg w-50 block ">
+            Save changes
+          </button>
+        ) : (
+          <button type="submit" className="btn btn-primary btn-lg w-50 block ">
+            Register
+          </button>
+        )}
       </form>
     </div>
   );
--- src/models/User.tsx
+++ src/models/User.tsx
@@ -1,13 +1,13 @@
 interface User {
-  _id: string;
+  _id?: string;
   firstName: string;
   lastName: string;
   email: string;
   role: string;
   phoneNumber: string;
   address: string;
-  password: string;
-  confirmPassword: string;
+  password?: string;
+  confirmPassword?: string;
 }
 
 export default User;
